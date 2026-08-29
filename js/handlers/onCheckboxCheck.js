b1b877a24278c005ea66d8ef8f7243e11867adc5
LeandroChavesM <leandrochavesmata@hotmail.com>
2026-08-29 19:58:09 -0300

    feat: add multi-profile support to userData

--- js/handlers/onCheckboxCheck.js
+++ js/handlers/onCheckboxCheck.js
@@ -1,17 +1,18 @@
 import { userData } from "../data/userData.js";
 import { saveUserData } from "../services/storage.js";
+import getCurrentProfile from "../services/getCurrentProfile.js";
 
 function onCheckboxCheck(checkbox, objective) {
-  if (!userData.objectives[objective]) {
-    userData.objectives[objective] = { status: "pending" };
+  if (!getCurrentProfile().objectives[objective]) {
+    getCurrentProfile().objectives[objective] = { status: "pending" };
   }
 
-  if (userData.objectives[objective].status === "completed") {
+  if (getCurrentProfile().objectives[objective].status === "completed") {
     checkbox.checked = true;
   }
 
   checkbox.addEventListener("change", () => {
-    userData.objectives[objective] = checkbox.checked
+    getCurrentProfile().objectives[objective] = checkbox.checked
       ? { status: "completed" }
       : { status: "pending" };
     saveUserData();
--- js/services/storage.js
+++ js/services/storage.js
@@ -1,4 +1,6 @@
 import { userData } from "../data/userData.js";
+import getCurrentProfile from "./getCurrentProfile.js";
+import { state } from "../state.js";
 
 export function saveUserData() {
   localStorage.setItem("userData", JSON.stringify(userData));
@@ -7,6 +9,7 @@ export function saveUserData() {
 export function loadUserData() {
   const saved = JSON.parse(localStorage.getItem("userData"));
   if (saved) {
-    userData.objectives = saved.objectives;
+    getCurrentProfile().objectives =
+      saved.profiles[state.currentProfile].objectives;
   }
 }
