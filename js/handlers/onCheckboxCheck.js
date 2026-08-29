66f20ab0e3d50bbe46621e23c359baf8babefa00
LeandroChavesM <leandrochavesmata@hotmail.com>
2026-08-29 17:02:26 -0300

    fix: prevent crash when objective has no userData entry

--- js/handlers/onCheckboxCheck.js
+++ js/handlers/onCheckboxCheck.js
@@ -1,11 +1,13 @@
 import { userData } from "../data/userData.js";
 
 function onCheckboxCheck(checkbox, objective) {
+  if (!userData.objectives[objective]) {
+    userData.objectives[objective] = { status: "pending" };
+  }
+
   if (userData.objectives[objective].status === "completed") {
     checkbox.checked = true;
   }
-  
-  userData.objectives[objective] = { status: "pending" };
 
   checkbox.addEventListener("change", () => {
     userData.objectives[objective] = checkbox.checked
